9292cb0ebda118520c24f216f232f918c6648a3e
Jacob Reynolds <jacob.e.reynolds@gmail.com>
2016-01-31 17:11:17 -0800

    add tslint

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var del = require('del');
 var gulp = require('gulp');
 var ts = require('gulp-typescript');
 var changed = require('gulp-changed');
+var tslint = require('gulp-tslint');
 
 var BUILD_FOLDER = './build';
 
@@ -25,4 +26,10 @@ gulp.task('typescript', function() {
 
   return tsResult.js
          .pipe(gulp.dest(BUILD_FOLDER));
+});
+
+gulp.task('tslint', [], function() {
+  return gulp.src(['src/**/*.ts'])
+         .pipe(tslint())
+         .pipe(tslint.report('prose', { emitError: false }));
 });
